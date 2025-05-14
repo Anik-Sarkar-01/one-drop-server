539dd4ed72636f9c05884667e472963248665a4b
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-05-15 00:21:54 +0600

    get api endpoint to get all the pending request added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -80,6 +80,22 @@ async function run() {
             const result = await donationRequestsCollection.find().toArray();
             res.send(result);
         })
+
+        app.get('/pending-donation-requests', async (req, res) => {
+            const status = req.query.status;
+            console.log(status);
+            const query = { donationStatus: status }
+            const result = await donationRequestsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/request-details/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await donationRequestsCollection.findOne(query);
+            res.send(result);
+        })
+
 
         app.get("/donation-requests/:email", async (req, res) => {
             const email = req.params.email;
