44d0b367a880b419fe7fbbabdc18871444c5d077
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-05-18 22:05:36 +0600

    review completed and final review

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
-var cors = require('cors')
+const cors = require('cors')
+// const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -22,13 +23,35 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const database = client.db("oneDropDB");
         const usersCollection = database.collection("users");
         const donationRequestsCollection = database.collection("donationRequests");
         const blogCollection = database.collection("blogs");
         const featuredDonorsCollection = database.collection("featuredDonors")
 
+        // // jwt related api
+        // app.post("/jwt", async (req, res) => {
+        //     const user = req.body;
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1hr" })
+        //     res.send({ token });
+        // })
+
+        // const verifyToken = (req, res, next) => {
+        //     console.log("Inside verify token", req.headers.authorization);
+        //     if (!req.headers.authorization) {
+        //         return res.status(401).send({ message: "unauthorized access" });
+        //     }
+        //     const token = req.headers.authorization.split(" ")[1];
+
+        //     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        //         if (err) {
+        //             return res.status(401).send({ message: "unauthorized access" })
+        //         }
+        //         req.decoded = decoded;
+        //         next();
+        //     })
+        // }
 
         // user related apis
         app.get("/users", async (req, res) => {
@@ -245,7 +268,7 @@ async function run() {
         })
 
         // featured donors related api
-         app.get("/featured-donors", async (req, res) => {
+        app.get("/featured-donors", async (req, res) => {
             const result = await featuredDonorsCollection.find().toArray();
             res.send(result);
         })
@@ -253,7 +276,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
