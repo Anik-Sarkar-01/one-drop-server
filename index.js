2b7dc48919b9419cc6e9453112ebc0897a67c66b
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-05-18 22:54:17 +0600

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
-// const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -29,29 +28,6 @@ async function run() {
         const donationRequestsCollection = database.collection("donationRequests");
         const blogCollection = database.collection("blogs");
         const featuredDonorsCollection = database.collection("featuredDonors")
-
-        // // jwt related api
-        // app.post("/jwt", async (req, res) => {
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1hr" })
-        //     res.send({ token });
-        // })
-
-        // const verifyToken = (req, res, next) => {
-        //     console.log("Inside verify token", req.headers.authorization);
-        //     if (!req.headers.authorization) {
-        //         return res.status(401).send({ message: "unauthorized access" });
-        //     }
-        //     const token = req.headers.authorization.split(" ")[1];
-
-        //     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        //         if (err) {
-        //             return res.status(401).send({ message: "unauthorized access" })
-        //         }
-        //         req.decoded = decoded;
-        //         next();
-        //     })
-        // }
 
         // user related apis
         app.get("/users", async (req, res) => {
